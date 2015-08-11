83878869ded9aeb41787e0183864a1714e264216
Erik Jorgensen <jorgie@missouri.edu>
2015-08-11 11:18:18 -0500

    Clean up and better handling of redirects. Also now support STDIN and multiple URLs on the commandline.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Text;
 
@@ -9,34 +10,43 @@ namespace TestWebURL
   {
     static void Main(string[] args)
     {
-      if (args.Length < 1 || args[0].Length < 3)
+      if (args.Length < 1)
       {
-        Console.WriteLine("You must provide a URL on the command line.");
-        Environment.Exit(99);
+        StreamReader sr = new StreamReader(Console.OpenStandardInput());
+        while (!sr.EndOfStream)
+          CheckURL(sr.ReadLine());
       }
+      else
+      {
+        string s = args[0].ToLower().Trim("-/".ToCharArray());
+        if(s == "help" || s == "h" || s == "?")
+        {
+          Console.WriteLine("TestWebUrl - jorgie@missouri.edu - 2015");
+          Console.WriteLine("  - Preforms some simples tests on URLs");
+          Console.WriteLine("  - Pass one or more URLs on the commandline or via STDIN.");
+          Console.WriteLine();
+          return;
+        }
 
-      string url = args[0];
-
-      if (!url.ToLower().StartsWith("http")) url = "http://" + url;
-
-      Console.WriteLine(CheckURL(url));
-
-
+        for (int x = 0; x < args.Length; x++)
+          CheckURL(args[x]);
+      }
     }
 
-    static string CheckURL(string URL)
+    private static void CheckURL(string URL)
     {
-      List<string> sb = new List<string>();
-      sb.Add(URL);
+      URL = URL.Trim();
+      if (URL == "") return;
+      if (!URL.ToLower().StartsWith("http")) URL = "http://" + URL;
+
+      Console.Write(URL);
 
       Uri uri = null;
       try { uri = new Uri(URL); }
       catch { }
 
       if (uri == null)
-      {
-        sb.Add("BAD_URI");
-      }
+        Console.Write(", BAD_URI");
       else
       {
         IPAddress[] ips = null;
@@ -46,24 +56,25 @@ namespace TestWebURL
 
         if (ips == null)
         {
-          sb.Add("DNS_FAIL");
+          Console.Write(", DNS_FAIL");
         }
         else
         {
-          sb.Add(GetHead(uri));
-          sb.Add(GetString(uri));
+          Console.Write(", " + DoHead(uri));
+          Console.Write(", " + DoGet(uri));
         }
       }
 
-      return string.Join(", ", sb.ToArray());
+      Console.WriteLine();
     }
 
-    static string GetHead(Uri URI)
+    static string DoHead(Uri URI)
     {
       WebRequest wr = WebRequest.Create(URI);
       wr.Method = "HEAD";
       HttpWebResponse res = null;
       string msg = "";
+      string lastMod = "";
 
       DateTime sTime = DateTime.Now;
 
@@ -76,37 +87,51 @@ namespace TestWebURL
 
       TimeSpan delta = DateTime.Now.Subtract(sTime);
 
-      if (res != null) msg = res.StatusCode.ToString();
-      return string.Format("HEAD:{0}/{1}ms", msg, delta.TotalMilliseconds);
+      if (res != null)
+      {
+        msg = string.Format("{0} {1}", (int)res.StatusCode, res.StatusDescription);
+        lastMod = res.LastModified.ToString("u").Replace(" ", "_").TrimEnd('Z');
+      }
+      return string.Format("HEAD:{0}/{1}/{2}ms", msg, lastMod, delta.TotalMilliseconds);
 
     }
 
-    static string GetString(Uri URI)
+    static string DoGet(Uri URI)
     {
-      WebRequest wr = WebRequest.Create(URI);
+      HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(URI);
       wr.Method = "GET";
-      
+      wr.AllowAutoRedirect = false;
+      wr.AuthenticationLevel = System.Net.Security.AuthenticationLevel.None;
+
       HttpWebResponse res = null;
-      string msg = "";
-      long length = 0;
 
       DateTime sTime = DateTime.Now;
 
       try { res = (HttpWebResponse)wr.GetResponse(); }
-      catch (WebException we)
-      {
-        res = (HttpWebResponse)we.Response;
-        msg = we.Status.ToString();
-      }
+      catch (WebException we) { res = (HttpWebResponse)we.Response; }
 
       TimeSpan delta = DateTime.Now.Subtract(sTime);
 
-      if (res != null) { 
-        msg = res.StatusCode.ToString();
+      string msg = "";
+      long length = 0;
+      string lastMod = "";
+
+      if (res != null) {
+        msg = string.Format("{0} {1}", (int)res.StatusCode, res.StatusDescription);
         length = res.ContentLength;
+        if (res.StatusCode == HttpStatusCode.Moved ||
+            res.StatusCode == HttpStatusCode.MovedPermanently ||
+            res.StatusCode == HttpStatusCode.Found ||
+            res.StatusCode == HttpStatusCode.Redirect ||
+            res.StatusCode == HttpStatusCode.RedirectKeepVerb ||
+            res.StatusCode == HttpStatusCode.RedirectMethod ||
+            res.StatusCode == HttpStatusCode.TemporaryRedirect
+            )
+          msg = string.Format("{0}/[{1}]", msg, res.GetResponseHeader("location"));
+        lastMod = res.LastModified.ToString("u").Replace(" ", "_").TrimEnd('Z');
       }
 
-      return string.Format("GET:{0}/{1}ms/{2}bytes", msg, delta.TotalMilliseconds, length);
+      return string.Format("GET:{0}/{1}/{2}ms/{3}bytes", msg, lastMod, delta.TotalMilliseconds, length);
 
     }
 
